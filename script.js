daaf2b8adbba829022e6651177efcf8f8ab3b8ce
Luke Harris <luke+git@lkhrs.com>
2024-07-22 12:28:53 -0500

    Make address lowercase

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 			formattedString += inputString.substr(i, 2);
 		}
+		formattedString = formattedString.toLowerCase();
 		return formattedString;
 	}
 
